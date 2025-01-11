05495f3e2d7686be56f72cbef5ab1b571bd01aab
ankit00010 <ankitmish83@gmail.com>
2025-01-11 12:44:26 +0530

    first commit

---
+++ src/context/admin_context.tsx
@@ -0,0 +1,17 @@
+import React, { createContext, ReactNode } from "react";
+
+export interface AdminContextType {}
+
+export const AdminContext = createContext<AdminContextType | null>(null);
+
+const AdminProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const admin_context_value = {};
+
+  return (
+    <AdminContext.Provider value={admin_context_value}>
+      {children}
+    </AdminContext.Provider>
+  );
+};
+
+export default AdminProvider;
